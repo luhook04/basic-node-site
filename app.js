a5a1e205ca8c49e2cc1e6ad0d49d21cdbb6cb4aa
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-06-20 14:30:57 -0400

    Rewrite app using express

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/index.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/about.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
+});
+
+app.get("/contact-me.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
